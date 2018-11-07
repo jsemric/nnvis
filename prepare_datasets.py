86e6ee661fb12456d54401cbbf580ff2df1ead33
jsemric <jakubsemric@gmail.com>
2018-11-07 17:34:35 +0000

    regression task

--- prepare_datasets.py
+++ prepare_datasets.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
 
 import os
+import urllib
+import zipfile
 import numpy as np
+import pandas as pd
+
 try:
     from tensorflow.keras.datasets import cifar10
 except ImportError:
     import tensorflow as tf
     cifar10 = tensorflow.keras.datasets.cifar10
 
-def get_cifar10_4():
+def load_cifar4():
     dir_path = os.path.join('data','cifar10_4')
 
     if not os.path.isdir(dir_path):
@@ -27,8 +31,9 @@ def get_cifar10_4():
 
     return x_train, y_train, x_val, y_val
 
-def prepare_cifar10(classes=None):
+def load_cifar10(classes=None):
     (x_train, y_train), (x_val, y_val) = cifar10.load_data()
+
     if classes is not None:
         train_idx = np.array([i in classes for i in y_train])
         x_train = x_train[train_idx]
@@ -38,3 +43,26 @@ def prepare_cifar10(classes=None):
         y_val = y_val[val_idx]
 
     return x_train, y_train, x_val, y_val
+
+def load_sgemm():
+    csv_path = os.path.join('data','sgemm_product.csv')
+
+    if not os.path.exists(csv_path):
+        print('Downloading dataset ...')
+        url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/00440/'\
+            'sgemm_product_dataset.zip'
+
+        os.makedirs('data', exist_ok=True)
+        path = os.path.join('data','sgemm_product_dataset.zip')
+        urllib.request.urlretrieve(url, path)
+
+        print('Extracting files ...')
+        with zipfile.ZipFile(path) as f:
+            f.extractall('data')
+
+    df = pd.read_csv(csv_path)
+    df['target'] = df.iloc[:,-4:].mean(axis=1)
+    X = df.iloc[:,:-5].values
+    y = df.target.values
+
+    return X.astype(float), y
---
+++ sgemm_model.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+import argparse
+import os
+
+from tensorflow import keras
+from sklearn.model_selection import train_test_split
+
+from prepare_datasets import load_sgemm
+from collector import Collector
+
+def main():
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('-e', '--epochs', type=int, default=10, metavar='N',
+        help='number of epochs (default 10)')
+    parser.add_argument('-b', '--batch-size', type=int, default=128,
+        metavar='N', help='batch size (default 128)')
+    parser.add_argument('-o', '--out', type=str,
+        help='output name, default sgemm', default='sgemm')
+    parser.add_argument('-s','--summary', action='store_true',
+        help='print the model summary and exit')
+    parser.add_argument('-v','--verbose', type=int, default=1,
+        help='control verbosity of training (default 1)')
+    args = parser.parse_args()
+
+    # load datasets
+    X, y = load_sgemm()
+
+    # scale
+    X -= X.min(axis=0)
+    X /= X.max(axis=0)
+
+    x_train, x_val, y_train, y_val = train_test_split(X, y, test_size=.2)
+
+    # build and train model
+    model = keras.models.Sequential([
+        keras.layers.Dense(64, activation='relu',
+            input_shape=(x_train.shape[1],)),
+        keras.layers.Dropout(.5),
+        keras.layers.Dense(48, activation='relu'),
+        keras.layers.Dense(32, activation='relu'),
+        keras.layers.Dense(1)
+    ])
+
+    if args.summary:
+        print(model.summary())
+        return
+   
+    logs_keys = ['mean_absolute_error', 'val_mean_absolute_error', 'val_loss']
+    clt = Collector(logfile=args.out, logs_keys=logs_keys,
+        validation_data=(x_val,y_val))
+    callbacks = [clt]
+
+    model.compile(loss='mse', metrics=['mae'], optimizer='adam')
+    model.fit(x_train, y_train, validation_data=[x_val, y_val],
+        batch_size=args.batch_size, epochs=args.epochs, callbacks=callbacks,
+        verbose=args.verbose)
+
+if __name__ == '__main__':
+    main()
